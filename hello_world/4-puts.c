cbe0549b2d256cbeb6bb34c46415e15036dc1335
Your Name <you@example.com>
2026-07-14 20:30:58 -0400

    task 4 done

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succesfuly)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
