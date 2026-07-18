a9169ee258c63bb73c2a24667b04e3cc1d7fb6e7
Your Name <you@example.com>
2026-07-18 07:39:12 -0400

    task 0 done

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * Making a code than ran
+ *
+ * integer positive, equal and negative
+ */
+
+int main(void)
+{
+	int n = 5;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
