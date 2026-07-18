1585d12f127d886389f8c408cabd072d4b4b4144
Your Name <you@example.com>
2026-07-18 13:31:37 -0400

    task 2 done

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alf = 'a';
+
+	while (alf <= 'z')
+	{
+		putchar(alf);
+		alf++;
+	}
+	putchar('\n');
+
+	return (0);
+}
