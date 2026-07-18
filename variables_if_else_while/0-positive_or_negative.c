b26dba36a602fce0eec887558d461a8ae1f1864f
Your Name <you@example.com>
2026-07-18 08:02:09 -0400

    task 0 done

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,21 @@ int main(void)
 {
 	int n = 5;
 
-	if (n > 0)
+	while (1)
 	{
-		printf("%d is positive\n",n);
+		if (n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+		else if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		else
+		{
+			printf("%d is negative\n", n);
+		}
+		break;
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-
 	return (0);
 }
