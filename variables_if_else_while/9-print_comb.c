7e10320505c3c5ceeec2b84428e44a6fb667bef2
Your Name <you@example.com>
2026-07-19 08:36:32 -0400

    task 9 done

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,14 +12,14 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-	
+
 		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-    }
-	putchar('\n');
+	}
+		putchar('\n');
 
 	return (0);
 }
