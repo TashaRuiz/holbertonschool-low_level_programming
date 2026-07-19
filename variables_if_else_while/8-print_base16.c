fe6f5bc9df76fbb459d867f00dea5f0a5713a0ae
Your Name <you@example.com>
2026-07-19 08:12:59 -0400

    task 8 done

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		 putchar(num);
+	}
+
+	return (0);
+}
