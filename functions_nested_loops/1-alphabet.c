d182a41e331fe8fd42f65120b58a81513c3b11df
Your Name <you@example.com>
2026-07-19 22:29:28 -0400

    task 1 done

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+    int alf;
+
+    for (alf = 'a'; alf <= 'z'; alf++)
+        _putchar(alf);
+
+    _putchar('\n');
+}
