1b9fdee3a43c57fcba8831a3e29ae9e9674d1665
Your Name <you@example.com>
2026-07-19 23:53:05 -0400

    task 2 done

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int num, alf;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (alf = 'a'; alf <= 'z'; alf++)
+		{
+			_putchar(alf);
+		}
+
+		_putchar('\n');
+    }
+}
