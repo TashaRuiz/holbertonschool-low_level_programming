6e6e936dc481461ffcee60b57b510b85853b93fb
Your Name <you@example.com>
2026-07-21 08:50:32 -0400

    task 8 done

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Prints every minutes of the day
+ */
+
 void jack_bauera(void)
 {
 	int hour;
 	int minute;
-	
-	for(hour = 0; hour < 24; hour++)
+
+	for (hour = 0; hour < 24; hour++)
 	{
-		for(minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
