bca66641f13ef95b63cdf9aefda868e952a6dee2
Your Name <you@example.com>
2026-07-21 09:44:29 -0400

    task 10 done

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - add two integers
+ *@a: first
+ *@b: second
+ *
+ *return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	a = 5; b = 7;
+
+	return (a + b);
+}
+
