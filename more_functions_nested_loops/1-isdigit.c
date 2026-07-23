a113be336dff6580408c896f7923dba550a3f496
Your Name <you@example.com>
2026-07-23 09:37:43 -0400

    Write a function that prints a triangle, followed by a new line.
    Prototype: void print_triangle(int size);
    You can only use _putchar function to print
    Where size is the size of the triangle
    If size is 0 or less, the function should print only a new line
    Use the character # to print the triangle

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for digitals (0 through 9)
+ *@c: is the character to check
+ *
+ * Return: 1 if its digit, 0 otherwise
+ */
+
+int  isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
