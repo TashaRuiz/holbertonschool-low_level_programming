d7da50810a52a80da26e9e8f2acaf467c86141f0
Your Name <you@example.com>
2026-07-24 14:15:09 -0400

    Write a function that swaps the values of two integers.
    Prototype: void swap_int(int *a, int *b);

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
- *swap_int - swaps the value of two integers
- *@a: first integer pointer
- *@b: second integer pointer
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first integer pointer
+ * @b: second integer pointer
  *
- *Return: nothing
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
