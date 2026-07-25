c0df3aaf6604013f290611784bc21c60d60114ac
Your Name <you@example.com>
2026-07-25 11:03:48 -0400

    Write a function that convert a string to an integer.
    Prototype: int _atoi(char *s);
    The number in the string can be preceded by an infinite number of characters
    You need to take into account all the - and + signs before the number
    If there are no numbers in the string, the function must return 0
    You are not allowed to use long
    You are not allowed to declare new variables of type array
    You are not allowed to hard-code special values
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
    FYI: The standard library provides a similar function: atoi. Run man atoi to learn more.

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,30 +11,27 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int result = 0;
-	int found = 0;
+	unsigned int num = 0;
 
-	while (s[i] != '\0')
+	while (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r'))
+		i++;
+
+	if (s[i] == '-')
 	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found = 1;
-			while (s[i] >= '0' && s[i] <= '9')
-			{
-				result = result * 10 + (s[i] - '0');
-				i++;
-			}
-			break;
-		}
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+		i++;
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 
-	if (!found)
-		return (0);
+	if (sign == -1)
+		return (-num);
 
-	return (result * sign);
+	return (num);
 }
