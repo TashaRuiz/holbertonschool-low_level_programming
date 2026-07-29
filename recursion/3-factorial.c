350edf6a5fe88b410a7d11a6e159afd305fdcf45
Your Name <you@example.com>
2026-07-29 14:09:47 -0400

    Write a function that returns the factorial of a given number.
    Prototype: int factorial(int n);
    If n is lower than 0, the function should return -1 to indicate an error
    Factorial of 0 is 1

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,11 +8,11 @@
  */
 int facrorial(int num)
 {
-	if(num == 0)
-		return (1);
-
-	else if (num < 0)
+	if(num < 0)
 		return (-1);
+
+	else if (num == 0)
+		return (1);
 
 	else
 		return (num * factorial(num - 1));
