db01795fa31fa63d8ef8079f1a18ba0838f5efe8
Your Name <you@example.com>
2026-07-29 09:54:15 -0400

    Write a function that locates a character in a string.
    Prototype: char *_strchr(char *s, char c);
    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
    FYI: The standard library provides a similar function: strchr. Run man strchr to learn more.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c,
+ * or NULL if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
