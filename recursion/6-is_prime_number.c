76397b68cd2d33447ef45882b93ab0e464da5c53
Your Name <you@example.com>
2026-07-31 11:31:14 -0400

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
    Prototype: int is_prime_number(int n);

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * check_prime - checks if n is divisible by divisor
+ * @n: number to check
+ * @divisor: current divisor
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int check_prime(int num, int divisor)
+{
+	if (divisor * divisor > num)
+	{
+		return (1);
+	}
+	else if (num % divisor == 0)
+	{
+		return (0);
+	}
+	return (check_prime(num, divisor + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise returns 0
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	
+	return(check_prime(n, 2));
+}
