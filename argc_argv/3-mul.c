bed0627c2908b3c130bdd4fcf43906e182211478
Your Name <you@example.com>
2026-08-01 11:48:07 -0400

    Write a program that multiplies two numbers.
    Your program should print the result of the multiplication, followed by a new line
    You can assume that the two numbers and result of the multiplication can be stored in an integer
    If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - Multiplies two numbers passed as command-line arguments.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success, 1 on error.
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2;
