016e03fb4d457d552161e726f622e05eb94fbdf7
Your Name <you@example.com>
2026-08-01 09:11:01 -0400

    Write a program that prints the number of arguments passed into it.
    Your program should print a number, followed by a new line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+
+		printf("%n", argc -1, argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
