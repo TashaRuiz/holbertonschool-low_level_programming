3b766999d0135a2ddb8f56f1831f36e259e45acc
Your Name <you@example.com>
2026-08-01 08:55:48 -0400

    Write a program that prints its name, followed by a new line.
    If you rename the program, it will print the new name, without having to compile it again
    You should not remove the path before the name of the program

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++ )
+	for (i = 0; i < argc; i++)
 	{
-		printf ("%s", argv[i]);
+		printf("%s", argv[i]);
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
