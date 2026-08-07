794ced6c22b56dd802e4f1761d2550755e72c74b
Your Name <you@example.com>
2026-08-07 07:54:56 -0400

    Write a function that frees a 2 dimensional grid previously created by your alloc_grid function.
    Prototype: void free_grid(int **grid, int height);
    Note that we will compile with your alloc_grid.c file. Make sure it compiles.

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -15,9 +15,9 @@ void free_grid(int **grid, int height)
 
 	if (grid == NULL)
 	{
-		return (NULL);
+		return;
 	}
-	for (i = 0, i < height; i ++)
+	for (i = 0; i < height; i ++)
 	{
 		free(grid[i]);
 	}
