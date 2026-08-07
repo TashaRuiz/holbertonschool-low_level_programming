5d62e2c8a9b7de9adacb46dc5cb6916c0ea9d02a
Your Name <you@example.com>
2026-08-07 12:11:45 -0400

    Write a function that concatenates two strings.
    Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
    The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
    If the function fails, it should return NULL
    If n is greater or equal to the length of s2 then use the entire string s2
    if NULL is passed, treat it as an empty string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,37 +7,47 @@
  *string_nconcat - concatenates two strings
  *@s1: first string
  *@s2: second string
- *@n: maximum number of bytes from s2
+ *@n: maximum number of bytes
  *
  * Return: pointer to newly allocated string or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, len1 = 0, len2 = 0;
 	char *result;
-	unsigned int len1, len2, i;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (*(s1 + len1) != '\0')
+	{
+		len1++;
+	}
+	while (*(s2 + len2) != '\0')
+	{
+		len2++;
+	}
 
-	if (n > len2)
-		n = len2;
-
-	result = malloc(len1 + n + 1);
+	result = malloc(sizeof(char) * (len1 + n + 1));
 	if (result == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < len1; i++)
+	{
 		result[i] = s1[i];
-
-	for (i = 0; i < n; i++)
-		result[len1 + i] = s2[i];
-
-	result[len1 + n] = '\0';
-
+	}
+	for (j = 0; j < n; i++, j++)
+	{
+		result[i] = s2[j];
+	}
+	result[i] = '\0';
 	return (result);
 }
