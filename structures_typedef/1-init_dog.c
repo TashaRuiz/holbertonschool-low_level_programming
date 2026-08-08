43e7d2d77414600c6fa1331aa02fc80ef0ecb356
Your Name <you@example.com>
2026-08-08 08:35:04 -0400

    Write a function that initialize a variable of type struct dog
    Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *init_dog - initialies dog struct
+ *@d: the pointer to the dog structure
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ * Return: if no information
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
