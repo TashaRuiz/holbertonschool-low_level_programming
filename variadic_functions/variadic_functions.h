4777587dce31063a9e37ece3bacca78751d56861
Your Name <you@example.com>
2026-08-14 11:59:54 -0400

    variadic_functions header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -9,4 +10,9 @@ void print_char(va_list list);
 void print_int(va_list list);
 void print_float(va_list list);
 void print_str(va_list list);
+typedef struct check
+{
+  char *type;
+  void (*f)();
+} check;
 #endif
