1f1251b85cf1fad66cbfcfbf31b06537bb64afbc
Your Name <you@example.com>
2026-08-15 10:00:26 -0400

    Write a function that prints strings, followed by a new line.
    Prototype: void print_strings(const char *separator, const unsigned int n, ...);
    where separator is the string to be printed between the strings
    and n is the number of strings passed to the function
    You are allowed to use printf
    If separator is NULL, don't print it
    If one of the string is NULL, print (nil) instead
    Print a new line at the end of your function

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints the strings
+ * @separator: Is the string to be printed between the strings
+ * @n: Is the number of strings passed to the function
+ *
+ * Return: void, no need for
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+			printf("%s", str);
+	}
+	va_end(args);
+	printf("\n");
+}
