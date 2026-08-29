a824e20825dcc1fd10fef6e90b587651bd5f4b2b
Your Name <you@example.com>
2026-08-29 11:31:40 -0400

    main headers for the prototypes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
 #include <stddef.h>
+#include <lists.h>
 #include <stdio.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
@@ -9,10 +10,10 @@ size_t list_len(const list_t *h);
 /*list_t *add_node_end(list_t **head, const char *str);*/
 void free_list(list_t *head);
 
-typedef struct list_t
+typedef struct list_s
 {
     char *str;
     unsigned int len;
-    struct list_t *next;
+    struct list_s *next;
 }list_t;
 #endif
