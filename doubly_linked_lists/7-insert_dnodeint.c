f0f7f7650017fbf281e6be338c4e771516ba4f40
Your Name <you@example.com>
2026-09-01 11:50:05 -0400

    Write a function that inserts a new node at a given position.
    Prototype: dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
    where idx is the index of the list where the new node should be added. Index starts at 0
    Returns: the address of the new node, or NULL if it failed
    if it is not possible to add the new node at index idx, do not add the new node and return NULL
    Your files 2-add_dnodeint.c and 3-add_dnodeint_end.c will be compiled during the correction

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,20 +12,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *current;
-	unsigned int i;
+	unsigned int i = 0;
 
+	current = *h;
 	if (h == NULL)
 	{
 		return (NULL);
 	}
-
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-
-	current = *h;
-	i = 0;
 	while (current != NULL && i < idx - 1)
 	{
 		current = current->next;
@@ -48,9 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = current;
 	new_node->next = current->next;
-
 	current->next->prev = new_node;
 	current->next = new_node;
-
 	return (new_node);
 }
