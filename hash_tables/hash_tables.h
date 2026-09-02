1aa53eac8876978ec7e53af34a50ed13a2d341c8
Your Name <you@example.com>
2026-09-02 17:42:24 -0400

    the main headers

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -33,6 +34,7 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
