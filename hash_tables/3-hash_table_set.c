7f585c52b4b730ff3c61ab538f596f139873faf9
Your Name <you@example.com>
2026-09-02 22:57:48 -0400

    Write a function that adds an element to the hash table.
    Prototype: int hash_table_set(hash_table_t *ht, const char *key, const char *value);
    Where ht is the hash table you want to add or update the key/value to
    key is the key. key can not be an empty string
    and value is the value associated with the key. value must be duplicated. value can be an empty string
    Returns: 1 if it succeeded, 0 otherwise
    In case of collision, add the new node at the beginning of the list

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,52 +12,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node;
-	hash_node_t *current;
 	char *new_value;
 
-	if (ht == NULL || key == NULL || key[0] == '\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	current = ht->array[index];
-
-	while (current != NULL)
+	new_node = ht->array[index];
+	while (new_node != NULL)
 	{
-		if (strcmp(current->key, key) == 0)
+		if (strcmp(new_node->key, key) == 0)
 		{
 			new_value = strdup(value);
 			if (new_value == NULL)
 				return (0);
 
-			free(current->value);
-			current->value = new_value;
-
+			free(new_node->value);
+			new_node->value = new_value;
 			return (1);
 		}
-
-		current = current->next;
+		new_node = new_node->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
